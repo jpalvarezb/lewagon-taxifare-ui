3a6485eda6fb6adad5a8fc4fedf6b3f270acfcfa
JP Alvarez <jpabalto11@gmail.com>
2025-03-07 11:44:15 +0100

    committing changes

--- script.js
+++ script.js
@@ -1,11 +1,11 @@
-let taxiFareApiUrl = 'http://localhost:8001/predict'; // replace with your API endpoint
+let taxiFareApiUrl = 'https://taxifare-post-1028549514617.europe-west1.run.app/predict'; // replace with your API endpoint
 const centralCoordinates = [-74.00597, 40.71427]; // starting position [lng, lat]
 
 if (window.location.href.includes('https://taxifare.lewagon.com')) {
   taxiFareApiUrl = 'https://taxifare.lewagon.ai/predict';
 }
 
-mapboxgl.accessToken = 'pk.eyJ1Ijoia3Jva3JvYiIsImEiOiJja2YzcmcyNDkwNXVpMnRtZGwxb2MzNWtvIn0.69leM_6Roh26Ju7Lqb2pwQ';
+mapboxgl.accessToken = 'pk.eyJ1IjoianBhbHZhcmV6YiIsImEiOiJjbTd5bHFpemwwODI0MnFzYTJvY2lzNjVkIn0.QtMQnOVwQPpU1JbR8IUzDg';
 
 const displayMap = (start, stop) => {
   const map = new mapboxgl.Map({
